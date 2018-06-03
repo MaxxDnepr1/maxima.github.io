29cc579b843a41e90413840082624cca6539c85e
MaxxDnepr1 <maksim.paryhin@gmail.com>
2018-06-03 17:03:03 +0300

    Done cite

---
+++ _script.js
@@ -0,0 +1,17 @@
+var user;
+user = 'Maxx';
+var name = prompt("Name please");
+if (name !== user){
+
+alert ("Ухади!")
+    
+}
+
+var pass = prompt("Pass please: ");
+if (pass == 7789210){
+
+    alert ("Welcom Maxx!");
+}
+else {
+    alert ("No");
+}
